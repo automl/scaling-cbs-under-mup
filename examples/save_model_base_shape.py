6840b79040072264ab3c3ebdee4950e4425078df
TarekAbouChakra <tarek.abouchakra@hotmail.com>
2024-07-08 14:26:38 +0200

    coord_check implementation for mup width scaling

--- examples/save_model_base_shape.py
+++ examples/save_model_base_shape.py
@@ -1,5 +1,5 @@
 from litgpt.config import Config
-from mup import make_base_shapes
+from mup import get_shapes, make_base_shapes
 
 from scales.model import GPT_Scales
 
@@ -9,11 +9,12 @@ if __name__ == "__main__":
     # Create your base config
     base_config = Config(block_size=512, n_layer=3, n_head=2, vocab_size=50257, bias=True, n_embd=width)
 
-    # Create the delta config (ie config to scale towards)
-    delta_config = Config(block_size=512, n_layer=3, n_head=2, vocab_size=50257, bias=True, n_embd=width * 2)
+    # Create the delta config (ie change the scaling dimension)
+    delta_config = Config(block_size=512, n_layer=3, n_head=2, vocab_size=50257, bias=True, n_embd=width * 4)
 
-    # Instantiate the models
-    base_model = GPT_Scales(base_config)
-    delta_model = GPT_Scales(delta_config)
+    # Get model shapes
+    base_model = get_shapes(GPT_Scales(base_config, mup=True))
+    delta_model = get_shapes(GPT_Scales(delta_config, mup=True))
 
     make_base_shapes(base_model, delta_model, "width32.bsh")
+    print("Scaling shape saved!")
