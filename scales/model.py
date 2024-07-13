0b0f158e68d670207c638785c7e410a007ffc5d9
Samir Garibov <karibbov@gmail.com>
2024-07-13 18:26:21 +0200

    attn_weight, attn_bias use same device

--- scales/model.py
+++ scales/model.py
@@ -46,9 +46,9 @@ class CausalSelfAttention_Scales(CausalSelfAttention):
 
         L, S = q.size(-2), k.size(-2)
         scale_factor = 1 / math.sqrt(q.size(-1)) if scale is None else scale
-        attn_bias = torch.zeros(L, S, dtype=q.dtype)
+        attn_bias = torch.zeros(L, S, dtype=q.dtype, device=q.device)
         if mask is None:
-            temp_mask = torch.ones(L, S, dtype=torch.bool).tril(diagonal=0)
+            temp_mask = torch.ones(L, S, dtype=torch.bool, device=q.device).tril(diagonal=0)
             attn_bias.masked_fill_(temp_mask.logical_not(), float("-inf"))
             attn_bias.to(q.dtype)
 
